5572fc9e5baea76a0200fd6015d4b13dd5ab1392
Alfaroque Islam <alfaroque5@gmail.com>
2020-06-25 18:44:10 -0400

    markdown edit

--- pages/index.py
+++ pages/index.py
@@ -16,13 +16,11 @@ column1 = dbc.Col(
         dcc.Markdown(
             """
         
-            ## Your Value Proposition
-            Emphasize how the app will benefit users. Don't emphasize the underlying technology.
-            ✅ RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
-            ❌ RUN is the only intelligent running app that uses sophisticated deep neural net machine learning to make your run smarter because we believe in ML driven workouts.
+            ## T20I Cricket Winrates
+            This application predicts the win rates of International Cricket teams in T20 Cricket.
             """
         ),
-        dcc.Link(dbc.Button('Your Call To Action', color='primary'), href='/predictions')
+        dcc.Link(dbc.Button('Predictions', color='primary'), href='/predictions')
     ],
     md=4,
 )
