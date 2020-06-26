91b742e036545e1ada91b796a4eb2abd52342fa3
Alfaroque Islam <alfaroque5@gmail.com>
2020-06-26 06:42:46 -0400

    commit

--- pages/process.py
+++ pages/process.py
@@ -20,7 +20,7 @@ column1 = dbc.Col(
 
             """
         ),
-        html.Img(src='assets/permutation.PNG', 
+        html.Img(src='assets/permutation.PNG' width="10%" height="10%", 
 				 className='img-fluid')
 
     ],
